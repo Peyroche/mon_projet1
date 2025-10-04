6569801aeb1241202c1eee32c5214ef1bdecb600
Peyroche <fplacide66@gmail.com>
2025-10-04 15:25:55 +0200

    Correction indentation et encodage du mot de passe

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
 import os
 from urllib.parse import quote_plus
 
-password = quote_plus(os.environ.get("DB_PASSWORD"))
-
 class Config:
     SESSION_COOKIE_HTTPONLY = True
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
     WTF_CSRF_SECRET_KEY = os.environ.get("CSRF_SECRET_KEY")
 
+    # Encodage sécurisé du mot de passe
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
+    ENCODED_PASSWORD = quote_plus(DB_PASSWORD)
+
     SQLALCHEMY_DATABASE_URI = (
-    f"mysql+pymysql://{os.environ.get('DB_USER')}:{password}"
-    f"@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
-)
+        f"mysql+pymysql://{os.environ.get('DB_USER')}:{ENCODED_PASSWORD}"
+        f"@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -20,3 +22,5 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
